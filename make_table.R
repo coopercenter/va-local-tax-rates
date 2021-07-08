c723b30553e968a9433be7846f12cd09cd3dea2f
Arthur Small <arthursmalliii@gmail.com>
2021-07-08 08:20:35 -0400

    Add cols for locality type and short name
    Also: arrange data by locality type, name

--- make_table.R
+++ make_table.R
@@ -1,8 +1,18 @@
 library(tidyverse)
 
-# read in va local tax rates survey results from xlsx file
+# read in va local tax rates survey results from xlsx file; drop unnecessary cols
 path2xl   <- here::here("data", "TR2020inputdataMain.xlsx")
-tax_rates <- readxl::read_xlsx(path = path2xl)
+tax_rates <- readxl::read_xlsx(path = path2xl) %>% select(-(Language:phone))
+
+# Add columns for locality type and (shorter) name
+tax_rates %>% 
+  mutate(locality_type = case_when(str_ends(ExternalDataReference, "County") ~ "C",
+                                   str_ends(ExternalDataReference, "City")   ~ "I",
+                                   TRUE ~ "T")) %>%
+  mutate(locality_name = word(ExternalDataReference, 1, -2)) %>%
+  arrange(locality_type, locality_name) %>% 
+  relocate(ExternalDataReference, locality_name, locality_type) -> tax_rates
+
 
 # function to make a table
 make_table <- function(vars){
