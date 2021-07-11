e1665371dd8f71aa01a1b5ca061f339023980086
Arthur Small <arthursmalliii@gmail.com>
2021-07-11 09:58:12 -0400

    Add new reference columns to tax_rates tbl

--- make_table.R
+++ make_table.R
@@ -2,16 +2,22 @@ library(tidyverse)
 
 # read in va local tax rates survey results from xlsx file; drop unnecessary cols
 path2xl   <- here::here("data", "TR2020inputdataMain.xlsx")
-tax_rates <- readxl::read_xlsx(path = path2xl) %>% select(-(Language:phone))
+tax_rates_raw <- readxl::read_xlsx(path = path2xl) %>% select(-(Language:phone))
 
 # Add columns for locality type and (shorter) name
-tax_rates %>% 
-  mutate(locality_type = case_when(str_ends(ExternalDataReference, "County") ~ "C",
-                                   str_ends(ExternalDataReference, "City")   ~ "I",
-                                   TRUE ~ "T")) %>%
+tax_rates_raw %>% 
+  mutate(locality_type_ID = case_when(str_ends(ExternalDataReference, "County") ~ "C",
+                                     str_ends(ExternalDataReference, "City")   ~ "I",
+                                     TRUE ~ "T")) %>%
+  mutate(locality_type = case_when(str_ends(ExternalDataReference, "County") ~ "County",
+                                   str_ends(ExternalDataReference, "City")   ~ "City",
+                                   TRUE ~ "Town")) %>%
+  mutate(locality_group = case_when(str_ends(ExternalDataReference, "County") ~ "Counties",
+                                   str_ends(ExternalDataReference, "City")   ~ "Cities",
+                                   TRUE ~ "Towns")) %>%
   mutate(locality_name = word(ExternalDataReference, 1, -2)) %>%
-  arrange(locality_type, locality_name) %>% 
-  relocate(ExternalDataReference, locality_name, locality_type) -> tax_rates
+  arrange(locality_type_ID, locality_name) %>% 
+  relocate(ExternalDataReference, locality_name, locality_type, locality_type_ID, locality_group) -> tax_rates
 
 
 # function to make a table
